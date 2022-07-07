0a3981c1e8269d41396895a4c522241f15bd6b7c
Jacob Sampson <jacob.samps@gmail.com>
2022-07-07 15:40:52 +0000

    Update language handling

--- lib/core/services/prismic.ts
+++ lib/core/services/prismic.ts
@@ -8,14 +8,9 @@ const client = prismic.createClient(PRISMIC_ENDPOINT, {
   accessToken: PRISMIC_ACCESS_TOKEN,
 });
 
-const UPDATE_TYPES: Record<Language, string> = {
-  en: 'update',
-  es: 'update-es',
-};
-
 class PrismicService {
   static async updates({ language }: { language: Language }) {
-    return await client.getAllByType(UPDATE_TYPES[language] || 'update');
+    return await client.getAllByType('update', { lang: language });
   }
 
   static async report({ topic }: { language: Language; topic: Topic }) {
