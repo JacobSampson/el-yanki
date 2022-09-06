89481f6f11e0e86235884a4188f06dbfaf5e564e
Jacob Sampson <samps294@umn.edu>
2022-09-05 22:00:06 -0500

    Fix date formatting

--- lib/core/utils.ts
+++ lib/core/utils.ts
@@ -3,9 +3,13 @@ import { Language } from './types';
 const TIMESTAMP_FORMAT = 'yyyy-MM-DDTHH:mm:ss';
 
 export const formatDate = (date: string, language: Language = 'en-us') => {
-  return new Date(Date.parse(date as string)).toLocaleDateString(language, {
+  const dateLabel = new Date(Date.parse(date as string)).toLocaleDateString(language, {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
   });
+
+  if (dateLabel === "Invalid Date") return date;
+
+  return dateLabel;
 };
